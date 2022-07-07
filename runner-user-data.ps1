171267db2b750849572d85225a20bd154960ee4f
Bohdan Sydor <bohdan.sydor@ext.hellofresh.com>
2022-07-08 00:21:47 +0200

    Add GHA runner PS script

---
+++ runner-user-data.ps1
@@ -0,0 +1,6 @@
+$ghasecret  =   ((Get-SECSecretValue -SecretId ghatoken).SecretString| ConvertFrom-Json).ghatoken
+mkdir actions-runner; cd actions-runner
+Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.294.0/actions-runner-win-x64-2.294.0.zip -OutFile actions-runner-win-x64-2.294.0.zip
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.294.0.zip", "$PWD")
+./config.cmd --url https://github.com/bsydor/gha-example --token $ghasecret  --ephemeral   --unattended
+.\run.cmd
