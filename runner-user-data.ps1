aba9d2f124f9dfd517d52a7d2c052b6284e877fb
bsydor <bsydor@gmail.com>
2022-11-06 19:20:02 +0100

    Update runner-user-data.ps1

--- runner-user-data.ps1
+++ runner-user-data.ps1
@@ -4,3 +4,4 @@ Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.29
 Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.294.0.zip", "$PWD")
 ./config.cmd --url https://github.com/bsydor/gha-example --token $ghasecret  --ephemeral   --unattended
 .\run.cmd
+#
